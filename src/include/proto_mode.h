47aa61d4e2a5eb1b34b1db75e7c6b4ef1b82dfa3
xllfyh <xllfyh@163.com>
2018-06-13 11:17:29 +0000

    proto.h

--- src/include/proto_mode.h
+++ src/include/proto_mode.h
@@ -4,82 +4,107 @@
 #define DEFAULT_SERVER_IP               "192.168.100.225"
 
 //<==================register================>
-#define UNAME_SIZE	                32
-#define ZHUCE_STATUS_OK			    0
-#define ZHUCE_STATUS_ERROR          1
-#define ZHUCE_SERVER_IP             DEFAULT_SERVER_IP
-#define ZHUCE_SERVER_PORT           1234
+#define MAXSIZE	                        1024
+#define REGISTER_STATUS_OK			    0
+#define REGISTER_STATUS_ERROR           1
+#define REGISTER_SERVER_IP              DEFAULT_SERVER_IP
+#define REGISTER_SERVER_PORT            10001
 
-#define FEMALE                      0
-#define MALE                        1
 
-struct zhuce_st {
-    char uname[UNAME_SIZE];
-    char password[UNAME_SIZE];
-    int sex;
+
+struct user_st {
+    char action;
+    char uname[MAXSIZE];
+    char pwd[MAXSIZE];
+    char question[MAXSIZE];
+    char answer[MAXSIZE];
     int status;
 };
 //zhuce table
 //id(integer, uname text,  passwd text)
 
 //<==================login================>
-#define DENGLU_STATUS_OK             0
-#define DENGLU_STATUS_ERROR          1
-#define DENGLU_SERVER_IP             DEFAULT_SERVER_IP
-#define DENGLU_SERVER_PORT           1235
+#define LOGIN_STATUS_OK                 0
+#define LOGIN_STATUS_ERROR              1
+#define LOGIN_SERVER_IP                 DEFAULT_SERVER_IP
+#define LOGIN_SERVER_PORT               10002
 
-struct denglu_st {
+#define CLIENT_LOGIN                          0
+#define CLIENT_EXIT                           1
+
+#define 
+
+struct login_st {
+    char action;    // 登录/退出
     char uname[UNAME_SIZE];
     char password[UNAME_SIZE];
-    int status;
+    int  status;
 };
 
-//<=================save address===========>
-#define JIZHUDIZHI_SERVER_IP            DEFAULT_SERVER_IP
-#define JIZHUDIZHI_SERVER_PORT          1236
-#define JIZHUDIZHI_STATUS_OK            0
-#define JIZHUDIZHI_STATUS_ERROR         1
 
-struct jizhudizhi_st {
-   char uname[UNAME_SIZE];
-   int status;
-};
 
-//denglu table
-//denglu(id integer, uname text, addr blob)
 
-//<==================addFriend================>
-#define TIANJIAHAOYOU_STATUS_OK         0
-#define TIANJIAHAOYOU_STATUS_ERROR      1
-#define TIANJIAHAOYOU_SERVER_IP         DEFAULT_SERVER_IP
-#define TIANJIAHAOYOU_SERVER_PORT       1237
-#define TIANJIAHAOYOU_STATUS_FRIEND     2
-struct tianjiahaoyou_st {
+
+//<==================FriendManager================>
+#define FRIENDMANAGER_STATUS_OK         0
+#define FRIENDMANAGER_STATUS_ERROR      1
+#define FRIENDMANAGER_SERVER_IP         DEFAULT_SERVER_IP
+#define FRIENDMANAGER_SERVER_PORT       10004
+#define FRIENDMANAGER_STATUS_FRIEND     2
+
+#define 
+
+
+struct friendmanager_st {
+    char action;
     char src_uname[UNAME_SIZE];
     char uname[UNAME_SIZE];
     char name[UNAME_SIZE];//server write
     int status;
 };
 
-//hy_xxx table
-//hy_xxx(id integer, uname text, name text)
 
-//<==================talk================>
-#define LIAOTIAN_STATUS_OK              0
-#define LIAOTIAN_STATUS_ERROR           1
-#define LIAOTIAN_SERVER_IP              DEFAULT_SERVER_IP
-#define LIAOTIAN_SERVER_PORT            1238
-#define MSG_UNAME_SIZE                  1024
+
+//<==================msg================>
+#define MSG_STATUS_OK              0
+#define MSG_STATUS_ERROR           1
+#define MSG_SERVER_IP              DEFAULT_SERVER_IP
+#define MSG_SERVER_PORT            10003
+
+
+#define MSG_CLIENT_PORT            8888
+
+#define HAOYOU_ITEM                     2
+
+struct msg_st {
+    char src_uname[MAXSIZE];
+    char dst_uname[MAXSIZE];
+    char time[MAXSIZE];
+    char msg[MAXSIZE];
+    int status;
+};
+
+
+//<==================heart================>
+#define HEART_STATUS_OK              0
+#define HEART_STATUS_ERROR           1
+#define HEART_SERVER_IP              DEFAULT_SERVER_IP
+#define HEART_SERVER_PORT            10005
+
 
 #define LIAOTIAN_CLIENT_PORT            8888
 
 #define HAOYOU_ITEM                     2
 
-struct liaotian_st {
-    char src_uname[UNAME_SIZE];
-    char dst_uname[UNAME_SIZE];
-    char time[UNAME_SIZE];
-    char msg[MSG_UNAME_SIZE];
-    int status;
+struct heart_st {
+    char uname[MAXSIZE];   
+    char time[MAXSIZE];
+    int  status;
 };
+
+
 #endif // PROTOCAL_H
+
+
+
+
